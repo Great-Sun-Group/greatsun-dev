6a1b1a28edf29f8072f76e1e6c78b6f1690bd515
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-09-14 20:34:21 +0000

    better logging

--- avatar/avatarUp.py
+++ avatar/avatarUp.py
@@ -19,6 +19,13 @@ def setup_logger():
     formatter = logging.Formatter('%(asctime)s - %(message)s')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)
+    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+    
     return logger
 
 logger = setup_logger()
