07ed158517f0a5fdcc170b7c6b545ab38bc5cd03
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-09-19 04:28:33 +0000

    another try

--- startup.sh
+++ startup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Create a script to set up the avatar command
+cat << EOF > /usr/local/bin/setup-avatar.sh
+#!/bin/bash
+alias avatar="python3 /workspaces/greatsun-dev/avatar/avatar"
+EOF
+
+# Make the setup script executable
+chmod +x /usr/local/bin/setup-avatar.sh
+
 # Make sure the avatar script is executable
 chmod +x /workspaces/greatsun-dev/avatar/avatar
 
-# Add alias to .bashrc if it doesn't exist
-grep -qxF 'alias avatar="python3 /workspaces/greatsun-dev/avatar/avatar"' ~/.bashrc || echo 'alias avatar="python3 /workspaces/greatsun-dev/avatar/avatar"' >> ~/.bashrc
-
-# Source .bashrc to make the alias available immediately
-source ~/.bashrc
+# Add sourcing of the setup script to .bashrc if it doesn't exist
+grep -qxF 'source /usr/local/bin/setup-avatar.sh' ~/.bashrc || echo 'source /usr/local/bin/setup-avatar.sh' >> ~/.bashrc
 
 # Print a message
-echo "Avatar environment is ready. You can now use the 'avatar' command."
+echo "Avatar environment is ready. Please run 'source ~/.bashrc' to use the 'avatar' command."
