e2413f382283bd0979478b7e2e066d20aa4fa080
Ryan Watson <85743878+ryanlukewatson@users.noreply.github.com>
2024-09-20 06:42:10 +0000

    try some startup consolidation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Set up the development environment
 # Create a script to set up the avatar command
 cat << EOF > /usr/local/bin/setup-avatar.sh
 #!/bin/bash
@@ -15,5 +19,8 @@ chmod +x /workspaces/greatsun-dev/avatar/avatar
 # Add sourcing of the setup script to .bashrc if it doesn't exist
 grep -qxF 'source /usr/local/bin/setup-avatar.sh' ~/.bashrc || echo 'source /usr/local/bin/setup-avatar.sh' >> ~/.bashrc
 
-# Print a message
-echo "Avatar environment is ready. Please run 'source ~/.bashrc' to use the 'avatar' command."
+# Start the necessary services
+# Start the avatar
+python3 /workspaces/greatsun-dev/avatar/avatarUp.py
+
+echo "Development environment set up and services started."
